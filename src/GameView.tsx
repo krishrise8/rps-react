84b968fcf48241c304b93182984f1013a5d2738a
Krish <krish@Krishs-MacBook-Pro.local>
2020-05-20 19:02:14 -0400

    Add validation for player choices

--- src/GameView.tsx
+++ src/GameView.tsx
@@ -10,24 +10,56 @@ const GameView: React.FC<InputComponentProps> = ({Player1Input, Player2Input}) =
 
   const [player1Choice, setP1Choice] = useState<string>(Player1Input)
   const [player2Choice, setP2Choice] = useState<string>(Player2Input)
-  const [rpsResults, setRPSResults] = useState()
-
+  const [rpsResults, setRPSResults]= useState()
+  const [p1Error, setP1Error] = useState()
+  const [p2Error, setP2Error] = useState<string>()
+  
   enum choices{
-     'ROCK',
-     'PAPER',
-     'SCISSORS'
+    'ROCK', 'PAPER', 'SCISSORS'
   }
 
   async function score() {
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json', 'Accept': 'application/json', 'Access-Control-Allow-Origin': 'true'},
-      body: JSON.stringify({ player1Choice: player1Choice , player2Choice : player2Choice})
-    };
 
-    await fetch('http://localhost:8080/game/play', requestOptions)
-      .then(response =>  response.text())
-      .then(data => setRPSResults(data));
+    setP1Error('')
+    setP2Error('')
+    setRPSResults('')
+    setP1Choice('')
+    setP2Choice('')
+
+    let validChoices = handleValidation(player1Choice, player2Choice)
+
+    if(validChoices){
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', 'Accept': 'application/json', 'Access-Control-Allow-Origin': 'true'},
+        body: JSON.stringify({ player1Choice: player1Choice.toUpperCase() , player2Choice : player2Choice.toUpperCase()})
+      };
+
+      await fetch('http://localhost:8080/game/play', requestOptions)
+        .then(response =>  response.text())
+        .then(data => setRPSResults(data));
+    }
+  }
+
+  function handleValidation(player1Choice : string, player2Choice : string){
+
+    if(!(player1Choice.toUpperCase() in choices)){
+      setP1Error('Invalid throw - must be rock, paper, or scissors')
+    }
+
+    if(!(player2Choice.toUpperCase() in choices)){
+      setP2Error('Invalid throw - must be rock, paper, or scissors')
+    }
+
+    if(player1Choice === ""){
+      setP1Error('Required')
+    }
+
+    if(player2Choice === ""){
+      setP2Error('Required')
+    }
+
+    return (player1Choice.toUpperCase() in choices) && (player2Choice.toUpperCase() in choices)
     
   }
   
@@ -35,8 +67,10 @@ const GameView: React.FC<InputComponentProps> = ({Player1Input, Player2Input}) =
     <div> {Player1Input}{Player2Input}
       <h3> Player 1</h3>
       <input type={'text'} value={player1Choice} onChange={(e: any) => setP1Choice(e.target.value)} />
+      <div>{p1Error}</div>
       <h3> Player 2</h3>
       <input type={'text'} value={player2Choice} onChange={(e: any) => setP2Choice(e.target.value)} />
+      <div>{p2Error}</div>
       <br/>
       <button onClick={score}> Submit </button> 
       <h3>{rpsResults}</h3>
